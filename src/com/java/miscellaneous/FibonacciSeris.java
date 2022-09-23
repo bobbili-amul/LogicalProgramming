fdaa4deffdfc911d044634e8dcb24c59bbbf0f06
bob-5757 <95516383+bob-5757@users.noreply.github.com>
2022-09-23 13:37:31 +0530

    sss

--- src/com/java/miscellaneous/FibonacciSeris.java
+++ src/com/java/miscellaneous/FibonacciSeris.java
@@ -5,9 +5,6 @@ public class FibonacciSeris {
 	public static void main(String[] args) {
 		int a = 0, b = 1, c;
 		System.out.println(Integer.MAX_VALUE);
-		int r = 2147483647+111111;
-		System.out.println(r);
-		// 0 1 1 2
 		System.out.print(a + " " + b);
 		for (int i = 1; i <= 100; i++) {
 			c = a + b;
